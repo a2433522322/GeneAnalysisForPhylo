ebcc77cc69f5dd8010a1825e739ef46da4f7d7e4
a2433522322 <44809738+a2433522322@users.noreply.github.com>
2025-03-14 19:39:41 +0800

    Update extractByAnnotation.py

--- extractByAnnotation.py
+++ extractByAnnotation.py
@@ -1,28 +1,30 @@
 from Bio import SeqIO
 import os
 
-# need change
-result_file = os.getcwd()+"/annotation_statistics.txt"
-output_dir = os.getcwd()+"/out"
+
+result_file = "/DATA/annotation_statistics.txt"#statistic information
+output_dir = "/DATA/int8T/users/symsx/coe_research/ONLY_coe/out"#output folder
 os.makedirs(output_dir, exist_ok=True)
+
 
 sequences = {}
 with open(result_file, 'r') as f:
-    next(f)  
+    next(f)
     for line in f:
         category, annotation, count, files_count = line.strip().split('\t')
         count = int(count)
         files_count = int(files_count)
-        if count == 253 and files_count == 253:#condition
+        if files_count == 229:
             sequences[(category, annotation)] = []
 
-input_dir = os.getcwd()
+
+input_dir = '/DATA/ONLY_coe'#input folder which has gb files
 for file in os.listdir(input_dir):
     if file.endswith('.gb'):
         gb_file_name = os.path.splitext(file)[0]
-        added_sequences = set()  
         with open(os.path.join(input_dir, file), 'r') as handle:
             for record in SeqIO.parse(handle, 'genbank'):
+                dic = {}
                 for feature in record.features:
                     for key, value in feature.qualifiers.items():
                         for category_annotation in sequences:
@@ -32,16 +34,15 @@ for file in os.listdir(input_dir):
                                 end = feature.location.end
                                 seq_fragment = record.seq[start:end]
                                 location_str = str(feature.location)
-                                if 'complement' in location_str:  
-                                    seq_fragment = seq_fragment.reverse_complement()
-                                    seq_info = (gb_file_name, str(seq_fragment)) 
+                                if annotation in dic:
+                                    pass
                                 else:
-                                    seq_fragment = seq_fragment
+                                    if feature.strand == -1:  
+                                        seq_fragment = seq_fragment.reverse_complement()
                                     seq_info = (gb_file_name, str(seq_fragment))  
-                                if seq_info not in added_sequences: 
                                     sequences[category_annotation].append(seq_info)
-                                    added_sequences.add(seq_info) 
-
+                                    dic[annotation] = 0
+                                    
 
 for category_annotation, seqs in sequences.items():
     category, annotation = category_annotation
@@ -51,4 +52,4 @@ for category_annotation, seqs in sequences.items():
             f.write(f">{gb_file_name}\n")
             f.write(f"{seq}\n")
 
-print("All files have saved")
+print("All results saved.")
