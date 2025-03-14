401b7f1d6061452d74736d4beab117178c1b1aa1
a2433522322 <44809738+a2433522322@users.noreply.github.com>
2025-03-14 19:35:41 +0800

    Update statistic.py

--- statistic.py
+++ statistic.py
@@ -4,17 +4,14 @@ import glob
 import os
 import logging
 
-
 logging.basicConfig(level=logging.INFO, format='%(message)s', handlers=[
     logging.FileHandler("process_log.txt"),
     logging.StreamHandler()
 ])
 
-
 def process_gb_file(file_path):
-    
     annotations_in_file = defaultdict(lambda: defaultdict(int))
-    invalid_annotations = []  
+    invalid_annotations = [] 
     
 
     try:
@@ -23,7 +20,7 @@ def process_gb_file(file_path):
                 for feature in record.features:
                     if feature.type == 'gene':
                         try:
-                          
+                            
                             if feature.location.end < feature.location.start:
                                 invalid_annotations.append((file_path, feature))
                                 continue
@@ -39,13 +36,11 @@ def process_gb_file(file_path):
     
     return annotations_in_file, invalid_annotations
 
-annotations_count = defaultdict(lambda: defaultdict(int)) 
-annotations_files = defaultdict(lambda: defaultdict(int)) 
+annotations_count = defaultdict(lambda: defaultdict(int))  
+annotations_files = defaultdict(lambda: defaultdict(int))  
 invalid_annotations_list = []  
 
-
-input_dir = os.getcwd()
-
+input_dir = '/DATA/'# the folder which has gb files
 
 gb_files = glob.glob(os.path.join(input_dir, '*.gb'))  
 
@@ -53,23 +48,21 @@ gb_files = glob.glob(os.path.join(input_dir, '*.gb'))
 for file in gb_files:
     file_annotations, invalid_annotations = process_gb_file(file)
     invalid_annotations_list.extend(invalid_annotations)
-    
-    
+
     for annotation_type in file_annotations:
         for annotation, count in file_annotations[annotation_type].items():
             annotations_count[annotation_type][annotation] += count
             annotations_files[annotation_type][annotation] += 1
 
-
 output_file = 'annotation_statistics.txt'
-
+invalid_output_file = 'invalid_annotations.txt'
 
 with open(output_file, 'w') as f:
-    f.write("category\tannotation\ttimes\tfiles\n")
+    f.write("type\tannotation\tTotal counts\tFile counts\n")
     for annotation_type in annotations_count:
         for annotation in annotations_count[annotation_type]:
             f.write(f"{annotation_type}\t{annotation}\t{annotations_count[annotation_type][annotation]}\t{annotations_files[annotation_type][annotation]}\n")
 
 
 
-logging.info(f"Results have got!")
+logging.info(f"All results saved in {output_file}")
